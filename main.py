57410fe4af25aa185214d595c636524fb4de470d
Manish Pandeya <254650626+maniesh-lab@users.noreply.github.com>
2026-06-04 21:44:45 +0545

    feat: scrape, analyze, and visualize book category data

---
+++ main.py
@@ -0,0 +1,26 @@
+from scraper import scrape_books
+from processors import clean_data
+from processors import merge_datasets
+from visualizer import generate_charts
+
+def main():
+    mystery_raw = scrape_books("https://books.toscrape.com/catalogue/category/books/mystery_3/index.html")
+    classics_raw = scrape_books("https://books.toscrape.com/catalogue/category/books/classics_6/index.html")
+    thriller_raw= scrape_books("https://books.toscrape.com/catalogue/category/books/thriller_37/index.html")
+    philosophy_raw = scrape_books("https://books.toscrape.com/catalogue/category/books/philosophy_7/index.html")
+
+    mystery_clean = clean_data(mystery_raw)
+    classics_clean = clean_data(classics_raw)
+    thriller_clean = clean_data(thriller_raw)
+    philosophy_clean = clean_data(philosophy_raw)
+
+    combined_df = merge_datasets(mystery_clean, classics_clean,thriller_clean,philosophy_clean)
+
+    generate_charts(combined_df)
+
+    combined_df.to_csv("output/combined_books.csv", index=False)
+
+    print(f"Done. {len(combined_df)} books scraped.")
+
+if __name__ == "__main__":
+    main()
